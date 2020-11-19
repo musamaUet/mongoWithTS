ca4bfaedc430647acfa9457527c638ac72fe6857
ejaz <ejaz@codility.co>
2020-11-19 18:09:18 +0500

    first commit

---
+++ dist/main.71ad219ebefa5687856d.hot-update.js
@@ -0,0 +1,36 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "main";
+exports.ids = null;
+exports.modules = {
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+/***/ (() => {
+
+eval("throw new Error(\"Module parse failed: Invalid regular expression flag (42:9)\\nFile was processed with these loaders:\\n * ./node_modules/ts-loader/index.js\\nYou may need an additional loader to handle the result of these loaders.\\n|     return res.send('Hellow world with express');\\n| });\\n> app.get(/products’, productCRUD.getProductList);, app.post(/products’,productCRUD.createProduct);, app.post(/updateproduct’,productCRUD.updateroduct);, app.post(/deleteproduct’,productCRUD.deleteproduct);))));\\n| \");\n\n//# sourceURL=webpack://mongowithts/./src/index.ts?");
+
+/***/ })
+
+};
+exports.runtime =
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ 	"use strict";
+/******/ 
+/******/ 	/* webpack/runtime/getFullHash */
+/******/ 	(() => {
+/******/ 		__webpack_require__.h = () => "825a92f9d72ffb5c35ac"
+/******/ 	})();
+/******/ 	
+/******/ }
+;
